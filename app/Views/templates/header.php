40a470708bcbb01388ea014a5c1bd147c1995d6b
ladronbx <ladronbravovlc@gmail.com>
2024-03-14 18:57:56 +0100

    [feat] : create header.php and footer.php in app/Views/templates.

---
+++ app/Views/templates/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+    <h1><?= esc($title) ?></h1>
+
+    <!-- ¿Qué es esc()?
+    esc() función. Es una función global proporcionada por CodeIgniter para ayudar a prevenir ataques XSS -->
