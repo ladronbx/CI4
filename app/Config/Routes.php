0471a7136c69a3f03b963be426469b0de472575f
ladronbx <ladronbravovlc@gmail.com>
2024-03-14 18:54:33 +0100

    [feat] : create Pages controller with index() and view() methods.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Controllers\Home;
+use App\Controllers\Pages;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', Home::class, ['index']);
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
